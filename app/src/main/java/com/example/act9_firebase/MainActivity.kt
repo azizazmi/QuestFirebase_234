9f0bb90043c07f6631e697b231466570e32998aa
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2026-01-03 03:23:05 +0700

    feat: replace Greeting with DataSiswaApp in MainActivity

--- app/src/main/java/com/example/act9_firebase/MainActivity.kt
+++ app/src/main/java/com/example/act9_firebase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.act9_firebase.ui.theme.Act9_firebaseTheme
+import com.example.act9_firebase.view.controlNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Act9_firebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    DataSiswaApp(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
