390cda2f9843b2cb85564e28913edffb57015165
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2026-01-08 18:49:41 +0700

    feat: implement EditViewModel for updating student data

--- app/src/main/java/com/example/act9_firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/act9_firebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,47 @@
 package com.example.act9_firebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.act9_firebase.modeldata.DetailSiswa
+import com.example.act9_firebase.modeldata.UIStateSiswa
+import com.example.act9_firebase.repositori.RepositorySiswa
+import com.example.act9_firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel (savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStatedHandle ")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getDetailSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+                println("Update sukses: $idSiswa")
+            }catch (e: Exception){
+                println("Update error: ${e.message}")
+            }
+        }
+    }
+}
