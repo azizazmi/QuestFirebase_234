a45c49f0482343d8c7759d41bef8bc2e51332c71
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2026-01-08 18:30:31 +0700

    feat: implement DetailViewModel with UI state and CRUD operations

--- app/src/main/java/com/example/act9_firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/act9_firebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,58 @@
 package com.example.act9_firebase.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.act9_firebase.modeldata.Siswa
+import com.example.act9_firebase.repositori.RepositorySiswa
+import com.example.act9_firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+import kotlin.text.get
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    private val idSiswa: Long =
+        checkNotNull(savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()) {
+            "idSiswa tidak ditemukan di SavedStateHandle"
+        }
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: Exception) {
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+    suspend fun hapusSatuSiswa() {
+        try {
+            repositorySiswa.hapusSatuSiswa(idSiswa)
+            println("Sukses Hapus Data: $idSiswa")
+        } catch (e: Exception) {
+            println("Gagal Hapus Data: ${e.message}")
+        }
+    }
+}
